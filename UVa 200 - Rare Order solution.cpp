b7d1cff7412610e3e2b8e8d7508846173c606577
Mohamed Hamada <mohamed43628@gmail.com>
2018-06-10 01:48:28 +0200

    Update UVa 200 - Rare Order solution.cpp

--- UVa 200 - Rare Order solution.cpp
+++ UVa 200 - Rare Order solution.cpp
@@ -1,91 +1,97 @@
-#include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
 
-#include<fstream>
-#include<istream>
-#include<ostream>
-
-#include<vector>
-#include<deque>
-#include<set>
-#include<map>
-#include<queue>
-
-#include<string>
-#include<cstring>
-#include<sstream>
-
-#include<cmath>
-#include <stdlib.h>
-#include<iomanip>
-#include<algorithm>
-#include<assert.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-
-#define PI		3.14159265
-#define OO		1e9
-#define S       second
-#define F       first  
+#define PI			3.14159265
+#define OO			1e9
+#define SS       	second
+#define FF       	first  
+#define Trace(n)  	cout<< #n <<" = "<< n << endl; 
+#define ll			long long
+#define endl		"\n"
 
 
 int dx[] = { 0, 0, -1, 1, 1, 1, -1, -1 };
 int dy[] = { -1, 1, 0, 0, 1, -1, 1, -1 };
-//fstream f("output.txt", ios::in | ios::out);
 
 void fast()
 {
 	std::ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
 	cout.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
 }
 
-bool vis[26], appeared[26];
-vector<int> adj[26];
-vector<int> TopoSort;
+map<char, vector<char> > adj;
+vector<char>ans;
+bool vis[26];
+bool Exist[26];
 
-void dfs(int u)
+void topoSort(char c)
 {
-	vis[u] = true;
+	vis[c - 'A'] = true;
 
-	for (int i = 0; i < adj[u].size(); i++)
-	{
-		int v = adj[u][i];
-		if (!vis[v])
-			dfs(v);
-	}
+	for (int i = 0; i < adj[c].size(); i++)
+	if (!vis[adj[c][i] - 'A'])
+		topoSort(adj[c][i]);
 
-	TopoSort.push_back(u);
+	ans.push_back(c);
 }
+
+
 int main()
 {
+	fast();
+	string s;
+	vector<string>v;
 
-	string current, prev;
-
-	cin >> prev;
-	while (cin >> current && current != "#")
+	while (cin >> s)
 	{
-		for (int i = 0; i < min(current.size(), prev.size()); i++)
+		if (s != "#")
+			v.push_back(s);
+		else
 		{
-			if (current[i] != prev[i])
+			memset(vis, 0, sizeof vis);
+			memset(Exist, 0, sizeof Exist);
+
+			for (int i = 0; i < v.size() - 1; i++)
 			{
-				appeared[current[i] - 'A'] = true;
-				appeared[prev[i] - 'A'] = true;
+				string s1 = v[i];
+				string s2 = v[i + 1];
 
-				adj[prev[i] - 'A'].push_back(current[i] - 'A');
-				break;
+				int mn = min(s1.size(), s2.size());
+
+				for (int j = 0; j < mn; j++)
+				{
+					if (s1[j] != s2[j])
+					{
+						adj[s1[j]].push_back(s2[j]);
+
+						Exist[s1[j] - 'A'] = true;
+						break;
+					}
+				}
 			}
+
+			for (int i = 0; i < 26; i++)
+			if (Exist[i] && !vis[i])
+				topoSort('A' + i);
+
+			reverse(ans.begin(), ans.end());
+
+			for (int i = 0; i < ans.size(); i++)
+				cout << ans[i];
+
+			v.clear();
+			adj.clear();
+			ans.clear();
+			cout << endl;
 		}
-
-		prev = current;
 	}
-
-	for (int i = 0; i < 26; i++)
-	if (!vis[i] && appeared[i])
-		dfs(i);
-
-
-	for (int i = TopoSort.size() - 1; i >= 0; i--)
-		cout << char(TopoSort[i] + 'A');
-
-	cout << endl;
-}s
+}
